b8dc7e0cce586dbb9773d64471b4fe37e72d636c
Jason Mobarak <jam@reason2.(none)>
2013-02-20 19:16:40 -0700

    Initial commit

---
+++ watsonbox_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatsonboxRails::Application.config.secret_token = '08628cbbbad710e6551f92aa2659ade7f0ba9e78a2ea5be54061ccfd462f84f8cc98c8ebbd4eb84109d5e5a9e2df42fe177c7035baff2b9b1047edcbdc33ee1f'
